defd6364ae13f39c0935f72f9cafb89be01ac204
mst-mkt <s1f102303057@iniad.org>
2024-01-01 18:11:51 +0900

    add: 各ページへのviewsを追加

--- collect/urls.py
+++ collect/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
 
+import collect.views.friends as friends
+import collect.views.messages as messages
+import collect.views.notifications as notifications
+import collect.views.settings as settings
 import collect.views.top as top
 
 urlpatterns = [
     path("", top.index, name="index"),
+    path("friends/", friends.index, name="friends"),
+    path("notifications/", notifications.index, name="notifications"),
+    path("messages/", messages.index, name="messages"),
+    path("settings/", settings.index, name="settings"),
 ]
