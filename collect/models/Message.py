29b31ae51939522059f332d4c529e5900b290cfd
SoraINIAD <s1f102302810@iniad.org>
2024-01-03 12:49:39 +0900

    add: Message.pyモデルの作成

--- collect/models/Message.py
+++ collect/models/Message.py
@@ -1,1 +1,13 @@
 from django.db import models
+from django.conf import settings
+
+class Message(models.Model):
+    """メッセージテーブル"""
+
+    text = models.TextField()
+    posted_at = models.DateField()
+    sender = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='sent_messages')
+    receiver = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='received_messages')
+
+    def __str__(self):
+        return self.text
--- collect/models/__init__.py
+++ collect/models/__init__.py
@@ -1,2 +1,3 @@
 from collect.models.CustomUser import CustomUser, CustomUserManager
 from collect.models.Friend import Friend
+from collect.models.Message import Message
