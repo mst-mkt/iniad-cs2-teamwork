e3215e75cd9c22458c2f9dd9910e754a312b1ca0
mst-mkt <s1f102303057@iniad.org>
2024-01-04 02:28:16 +0900

    fix: Notificationモデルのファイル名を修正等

--- collect/models/__init__.py
+++ collect/models/__init__.py
@@ -1,3 +1,4 @@
 from collect.models.CustomUser import CustomUser, CustomUserManager
 from collect.models.Friend import Friend
 from collect.models.Message import Message
+from collect.models.Notification import Notification
