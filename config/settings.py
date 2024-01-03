2d7b9a37ded7e5024e0ec48093d430d1d4c3a877
mst-mkt <s1f102303057@iniad.org>
2024-01-03 13:08:56 +0900

    add: ログイン用の設定を追加

--- config/settings.py
+++ config/settings.py
@@ -126,3 +126,7 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "collect.CustomUser"
+
+LOGIN_URL = "login"
+LOGIN_REDIRECT_URL = "index"
+LOGOUT_REDIRECT_URL = "login"
