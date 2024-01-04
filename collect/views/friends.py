7a42bb1b934f33f3ec6621fc6b530b95fe57a9a3
soshirooo <S1F102302326@INIAD.ORG>
2024-01-05 02:35:26 +0900

    フレンド

--- collect/views/friends.py
+++ collect/views/friends.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 
+from collect.models import CustomUser, Friend
+
 
 def index(request):
-    return render(request, "collect/friends.html")
+    friends_as_user1 = Friend.objects.filter(user1=request.user, is_approved=True)
+    friends_as_user2 = Friend.objects.filter(user2=request.user, is_approved=True)
+    friend_users = list(friends_as_user1.values_list("user2", flat=True)) + list(
+        friends_as_user2.values_list("user1", flat=True)
+    )
+    friends = CustomUser.objects.filter(id__in=friend_users)
+    context = {"friends": friends}
+    return render(request, "collect/friends.html", context)
