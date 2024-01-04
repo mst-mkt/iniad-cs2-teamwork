86d2e06944664ef6988833c421a72978df7b4f4b
Buchi63975 <s1f102302405@iniad.org>
2024-01-04 18:00:12 +0900

    利用規約エラー改善

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.urls import path
+from config import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
