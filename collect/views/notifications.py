be8ec576394c11239a1ac4d392ba030d54209c39
mst-mkt <s1f102303057@iniad.org>
2024-01-05 06:50:00 +0900

    feat: 通知の表示を実装

--- collect/views/notifications.py
+++ collect/views/notifications.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
+from collect.models import Notification
+
 
 def index(request):
-    return render(request, "collect/pages/notifications.html")
+    notifications = Notification.objects.order_by("-posted_at")
+    context = {"notifications": notifications}
+    return render(request, "collect/pages/notifications.html", context)
