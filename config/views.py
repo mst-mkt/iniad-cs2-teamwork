2235338ccfe2722940d22454e0e7f9ffd21a6ba4
Buchi63975 <s1f102302405@iniad.org>
2024-01-04 18:25:32 +0900

    collect!!!!!!

--- config/views.py
+++ config/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 def terms_of_service(request):
     # 'terms_of_service.html' はあなたの利用規約テンプレートのファイル名です
-    return render(request, 'terms_of_service.html')
+    return render(request, 'collect/terms_of_service.html')
